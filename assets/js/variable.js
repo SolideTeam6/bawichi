6c8f4817f46eb7e9fb865e63981a8504ac6fce39
alexise26 <alexise26@gmail.com>
2017-04-05 00:22:10 -0600

    link solidemx, redes sociales

--- assets/js/variable.js
+++ assets/js/variable.js
@@ -35,7 +35,7 @@ var _animationDuration = 1750; // animation duration
 countdown
 ==============================================================================*/
 var _countdown = true; // countdown toggle
-var _countdownDate = '03/17/2017 11:59:59'; // 2015-12-24 23:59:59
+var _countdownDate = '04/07/2017 11:59:59'; // 2015-12-24 23:59:59
 var _countdownTimezone = '-7'; // timezone
 
 /*=================================================
